a082652955cd3d694acb562d97a6a7325dd8fe55
Manuel Odendahl <wesen@ruinwesen.com>
2011-08-25 16:05:35 +0200

    Fix columnview dispatcher

--- jquery.columnview.js
+++ jquery.columnview.js
@@ -194,13 +194,7 @@
     } else if ( typeof method === 'object' || ! method ) {
       return methods.init.apply( this, arguments );
     } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
-    }       if ( methods[method] ) {
-      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
-    } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+      $.error( 'Method ' +  method + ' does not exist on jQuery.columnview ');
     }    
   };
 
